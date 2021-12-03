d978b477ff8c2887465b757fa9ccdccb5c78db25
lifishake <11782494+lifishake@users.noreply.github.com>
2021-12-03 19:09:53 +0800

    读书的summary里增加出版社

--- bddb-main.php
+++ bddb-main.php
@@ -7,7 +7,7 @@
  * Description: 抵制源于喜爱。既然无法改变它，那就自己创造一个。
  * Author:      lifishake
  * Author URI:  http://pewae.com
- * Version:     0.0.4
+ * Version:     0.0.5
  * License:     GNU General Public License 3.0+ http://www.gnu.org/licenses/gpl.html
  */
 
