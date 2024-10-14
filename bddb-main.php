520b5f83cf2420a96112a8243f8043f21a60cde6
gao_wangzhong <gao_wangzhong@outlook.com>
2024-10-14 23:51:36 +0800

    修改了竖屏的唱片预览，特殊专辑预览的角标。

--- bddb-main.php
+++ bddb-main.php
@@ -7,8 +7,8 @@
  * Description: 抵制源于喜爱。既然无法改变它，那就自己创造一个。
  * Author:      lifishake
  * Author URI:  http://pewae.com
- * Version:     0.9.7
- * Date:        2024-10-10
+ * Version:     0.9.8
+ * Date:        2024-10-14
  * License:     GNU General Public License 3.0+ http://www.gnu.org/licenses/gpl.html
  */
 
