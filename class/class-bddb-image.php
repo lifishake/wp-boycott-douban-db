68c9d6b0f9bc6862f30a4123e39220781de6aa7a
gao_wangzhong <gao_wangzhong@outlook.com>
2026-02-24 16:57:28 +0800

    增加webp

--- class/class-bddb-image.php
+++ class/class-bddb-image.php
@@ -41,16 +41,12 @@ class Bddb_SimpleImage {
         
         $this->image_type = $image_info[2];
         if( $this->image_type == IMAGETYPE_JPEG ) {
-
             $this->image = imagecreatefromjpeg($filename);
         } elseif( $this->image_type == IMAGETYPE_GIF ) {
-
             $this->image = imagecreatefromgif($filename);
         } elseif( $this->image_type == IMAGETYPE_PNG ) {
-
             $this->image = imagecreatefrompng($filename);
         } elseif( $this->image_type == IMAGETYPE_WEBP ) {
-            
             $this->image = imagecreatefromwebp($filename);
         }
         if ($filename==="./temp") {
@@ -62,11 +58,11 @@ class Bddb_SimpleImage {
         if( $image_type == IMAGETYPE_JPEG ) {
             imagejpeg($this->image,$filename,$compression);
         } elseif( $image_type == IMAGETYPE_GIF ) {
-
             imagegif($this->image,$filename);
         } elseif( $image_type == IMAGETYPE_PNG ) {
-
             imagepng($this->image,$filename);
+        } elseif( $image_type == IMAGETYPE_WEBP ) {
+            imagewebp($this->image,$filename);
         }
         if( $permissions != null) {
 
@@ -78,11 +74,11 @@ class Bddb_SimpleImage {
         if( $image_type == IMAGETYPE_JPEG ) {
             imagejpeg($this->image);
         } elseif( $image_type == IMAGETYPE_GIF ) {
-
             imagegif($this->image);
         } elseif( $image_type == IMAGETYPE_PNG ) {
-
             imagepng($this->image);
+        } elseif( $image_type == IMAGETYPE_WEBP ) {
+            imagewebp($this->image);
         }
     }
     function getWidth() {
